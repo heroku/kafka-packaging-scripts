87ece7448eee4bc33030b3db3f6cee741f5fa439
Ewen Cheslack-Postava <me@ewencp.org>
2015-01-14 17:28:10 -0800

    Add final step to compile full distribution tar.gz and zip files.

--- package.sh
+++ package.sh
@@ -4,6 +4,10 @@ set -e
 set -x
 
 . versions.sh
+# All the packages except for Kafka, without the confluent prefix. Kafka needs
+# special handling because we support multiple Scala versions. These all need to
+# have the same version number currently.
+PACKAGES="common rest-utils kafka-rest"
 
 pushd repos
 for REPO in "http://git-wip-us.apache.org/repos/asf/kafka.git" "git@github.com:confluentinc/kafka-packaging.git" "git@github.com:confluentinc/common.git" "git@github.com:confluentinc/rest-utils.git" "git@github.com:confluentinc/kafka-rest.git"; do
@@ -72,3 +76,26 @@ vagrant ssh rpm -- cp /vagrant/build/kafka-rest-rpm.sh /tmp/kafka-rest-rpm.sh
 vagrant ssh rpm -- -t sudo VERSION=$CONFLUENT_VERSION BRANCH=$BRANCH SIGN=$SIGN /tmp/kafka-rest-rpm.sh
 vagrant ssh deb -- cp /vagrant/build/kafka-rest-deb.sh /tmp/kafka-rest-deb.sh
 vagrant ssh deb -- -t sudo VERSION=$CONFLUENT_VERSION BRANCH=$BRANCH SIGN=$SIGN /tmp/kafka-rest-deb.sh
+
+
+## COMPILED PACKAGES ##
+OUTPUT=`pwd`/output
+rm -rf /tmp/confluent-packaging
+mkdir -p /tmp/confluent-packaging
+pushd /tmp/confluent-packaging
+
+for SCALA_VERSION in $SCALA_VERSIONS; do
+    mkdir "confluent-${CONFLUENT_VERSION}"
+    pushd "confluent-${CONFLUENT_VERSION}"
+    tar -xz --strip-components 1 -f "${OUTPUT}/confluent-kafka-${KAFKA_VERSION}-${SCALA_VERSION}.tar.gz"
+    for PACKAGE in $PACKAGES; do
+        tar -xz --strip-components 1 -f "${OUTPUT}/confluent-${PACKAGE}-${CONFLUENT_VERSION}.tar.gz"
+    done
+    popd
+    tar -czf "${OUTPUT}/confluent-${CONFLUENT_VERSION}-${SCALA_VERSION}.tar.gz" "confluent-${CONFLUENT_VERSION}"
+    zip -r "${OUTPUT}/confluent-${CONFLUENT_VERSION}-${SCALA_VERSION}.zip" "confluent-${CONFLUENT_VERSION}"
+    rm -rf "confluent-${CONFLUENT_VERSION}"
+done
+
+popd
+rm -rf /tmp/confluent-packaging
