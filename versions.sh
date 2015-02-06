79df5788cb2a0b960073ff30aa6b6b72fec24f14
Ewen Cheslack-Postava <me@ewencp.org>
2015-02-06 11:57:03 -0800

    Update Camus branch to confluent-master.

--- versions.sh
+++ versions.sh
@@ -4,7 +4,7 @@ SCALA_VERSIONS="2.9.1 2.9.2 2.10.4 2.11.5"
 BRANCH="origin/master"
 # Branch overrides for specific projects. Use
 # project_name_using_underscores_BRANCH.
-camus_BRANCH="origin/avro-decoder"
+camus_BRANCH="origin/confluent-master"
 SIGN="yes"
 SIGN_KEY=""
 REVISION="1"
