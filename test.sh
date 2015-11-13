525fa8dc53e7fcb0bbd864963d3a7417cd7dbf53
Michael G. Noll <michael@confluent.io>
2015-11-13 22:50:41 +0100

    Make test.sh more robust against race conditions, ZK stop problems, and interrupted test runs

--- test.sh
+++ test.sh
@@ -42,10 +42,27 @@ test_zk_start() {
 
 test_zk_stop() {
     machine=$1
-    # zookeeper-server-stop finds more than one process because it only
+    # `zookeeper-server-stop` finds more than one process because it only
     # searches for 'zookeeper', so to prevent collateral damage we do this
     # manually in a way that filters more accurately to the right process.
-    vagrant ssh $machine -- "ps ax | grep java | grep -i QuorumPeerMain | grep -v grep | awk '{print \$1}' | sudo xargs kill -SIGINT"
+    #
+    # Also, there is apparently a problem when using `vagrant ssh` to launch
+    # a background process like we do in `test_zk_start` (FWIW we also tried
+    # command variants with e.g. nohup, I/O redirection, to fix the root cause
+    # but without success):  If you do, then sending a SIGINT is not enough to
+    # terminate the ZK process.
+    # SIGINT /is/ enough if you manually start ZK (with the same
+    # `sudo /usr/bin/zookeeper-server-start ...` command line) and then `kill -SIGINT`
+    # the ZK pid via `vagrant ssh`.  However, it does not work if the ZK process was
+    # started in the background from the host machine via `vagrant ssh`.
+    # This could be caused by interaction between vagrant, ssh, (no) terminal,
+    # or something else.
+    # For the time being we simply use the workaround to terminate the ZK process
+    # via SIGTERM.  This also means that we cannot use `zookeeper-server-stop`
+    # in this setup because that script kills via SIGINT.
+    #
+    # TODO: Figure out why SIGINT is not enough to kill the ZK process when we have started ZK via `vagrant ssh`.
+    vagrant ssh $machine -- "ps ax | grep java | grep -i QuorumPeerMain | grep -v grep | awk '{print \$1}' | sudo xargs kill -SIGTERM"
 }
 
 test_kafka_start() {
@@ -63,6 +80,11 @@ test_kafka_start() {
 test_kafka_stop() {
     machine=$1
     vagrant ssh $machine -- "sudo /usr/bin/kafka-server-stop"
+    # Prevent a race condition where the broker may not be fully shutdown yet
+    # but the ZK process already is.  If that should happen, then the broker
+    # will be stuck in a ZK reconnection loop and can only be terminated by a
+    # SIGKILL.
+    sleep 25
 }
 
 test_schema_registry() {
@@ -104,8 +126,20 @@ for SCALA_VERSION in $SCALA_VERSIONS; do
     #######
     # RPM #
     #######
+
+    # pre-test sanitization
+    vagrant ssh rpm -- sudo rpm --erase confluent-camus || true
+    vagrant ssh rpm -- sudo rpm --erase confluent-kafka-connect-jdbc || true
+    vagrant ssh rpm -- sudo rpm --erase confluent-kafka-rest || true
+    vagrant ssh rpm -- sudo rpm --erase confluent-schema-registry || true
+    vagrant ssh rpm -- sudo rpm --erase confluent-rest-utils || true
+    vagrant ssh rpm -- sudo rpm --erase confluent-common || true
+    for LOCAL_SCALA_VERSION in $SCALA_VERSIONS; do
+        vagrant ssh rpm -- sudo rpm --erase confluent-kafka-${LOCAL_SCALA_VERSION} || true
+    done
     vagrant ssh rpm -- sudo rm -rf $KAFKA_DATA_DIR
     vagrant ssh rpm -- sudo rm -rf $ZOOKEEPER_DATA_DIR
+
     vagrant ssh rpm -- sudo rpm --install /vagrant/output/confluent-kafka-${SCALA_VERSION}-*.noarch.rpm
     vagrant ssh rpm -- sudo rpm --install /vagrant/output/confluent-common-*.rpm
     vagrant ssh rpm -- sudo rpm --install /vagrant/output/confluent-rest-utils-*.rpm
@@ -123,6 +157,7 @@ for SCALA_VERSION in $SCALA_VERSIONS; do
     test_kafka_stop rpm
     test_zk_stop rpm
 
+    # post-test sanitization
     vagrant ssh rpm -- sudo rpm --erase confluent-camus
     vagrant ssh rpm -- sudo rpm --erase confluent-kafka-connect-jdbc
     vagrant ssh rpm -- sudo rpm --erase confluent-kafka-rest
@@ -131,12 +166,23 @@ for SCALA_VERSION in $SCALA_VERSIONS; do
     vagrant ssh rpm -- sudo rpm --erase confluent-common
     vagrant ssh rpm -- sudo rpm --erase confluent-kafka-${SCALA_VERSION}
 
-
     #######
     # DEB #
     #######
+
+    # pre-test sanitization
+    vagrant ssh deb -- sudo dpkg --purge confluent-camus || true
+    vagrant ssh deb -- sudo dpkg --purge confluent-kafka-connect-jdbc || true
+    vagrant ssh deb -- sudo dpkg --purge confluent-kafka-rest || true
+    vagrant ssh deb -- sudo dpkg --purge confluent-schema-registry || true
+    vagrant ssh deb -- sudo dpkg --purge confluent-rest-utils || true
+    vagrant ssh deb -- sudo dpkg --purge confluent-common || true
+    for LOCAL_SCALA_VERSION in $SCALA_VERSIONS; do
+        vagrant ssh deb -- sudo dpkg --remove confluent-kafka-${LOCAL_SCALA_VERSION} || true
+    done
     vagrant ssh deb -- sudo rm -rf $KAFKA_DATA_DIR
     vagrant ssh deb -- sudo rm -rf $ZOOKEEPER_DATA_DIR
+
     vagrant ssh deb -- sudo dpkg --install /vagrant/output/confluent-kafka-${SCALA_VERSION}*_all.deb
     vagrant ssh deb -- sudo dpkg --install /vagrant/output/confluent-common*_all.deb
     vagrant ssh deb -- sudo dpkg --install /vagrant/output/confluent-rest-utils*_all.deb
@@ -154,11 +200,12 @@ for SCALA_VERSION in $SCALA_VERSIONS; do
     test_kafka_stop deb
     test_zk_stop deb
 
-    vagrant ssh deb -- sudo dpkg --remove confluent-camus
-    vagrant ssh deb -- sudo dpkg --remove confluent-kafka-connect-jdbc
-    vagrant ssh deb -- sudo dpkg --remove confluent-kafka-rest
-    vagrant ssh deb -- sudo dpkg --remove confluent-schema-registry
-    vagrant ssh deb -- sudo dpkg --remove confluent-rest-utils
-    vagrant ssh deb -- sudo dpkg --remove confluent-common
-    vagrant ssh deb -- sudo dpkg --remove confluent-kafka-${SCALA_VERSION}
+    # post-test sanitization
+    vagrant ssh deb -- sudo dpkg --purge confluent-camus
+    vagrant ssh deb -- sudo dpkg --purge confluent-kafka-connect-jdbc
+    vagrant ssh deb -- sudo dpkg --purge confluent-kafka-rest
+    vagrant ssh deb -- sudo dpkg --purge confluent-schema-registry
+    vagrant ssh deb -- sudo dpkg --purge confluent-rest-utils
+    vagrant ssh deb -- sudo dpkg --purge confluent-common
+    vagrant ssh deb -- sudo dpkg --purge confluent-kafka-${SCALA_VERSION}
 done
