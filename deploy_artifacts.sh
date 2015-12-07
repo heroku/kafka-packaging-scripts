15b29e40a60fcd308cd284ab9870f9b5b76a0edc
Michael G. Noll <michael@confluent.io>
2015-12-07 08:59:38 +0100

    Use variables to configure build directories

--- deploy_artifacts.sh
+++ deploy_artifacts.sh
@@ -29,42 +29,51 @@ fi
 # (cf. https://github.com/jcaddel/maven-s3-wagon/wiki/Authentication)
 export AWS_SECRET_KEY="$AWS_SECRET_ACCESS_KEY"
 
+declare -r BUILD_ROOT="/tmp/confluent"
+declare -r KAFKA_BUILD_ROOT="$BUILD_ROOT/kafka-packaging"
+
+###
+### Kafka
+###
+
 # FIXME because of the way the Kafka build works, we can't actually specify
 # SCALA_LANGUAGES here. Also, we unfortunately need to store the info for
 # signing here, including the full password. To avoid clobbering the user's real
 # gradle.properties we use a fake gradle home
-mkdir -p /tmp/fakegradlehome
+FAKE_GRADLE_HOME="/tmp/fakegradlehome"
+mkdir -p $FAKE_GRADLE_HOME
 KEY_ID=`gpg --list-secret-keys $SIGN_KEY | grep -v secring | grep sec | awk -F' ' '{ print $2 }' | awk -F/ '{ print $2 }'`
 echo "Enter your GPG key password for $KEY_ID:"
 read -s PASSWORD
-cat <<EOF > /tmp/fakegradlehome/gradle.properties
+cat <<EOF > $FAKE_GRADLE_HOME/gradle.properties
 signing.keyId=$KEY_ID
 signing.password=$PASSWORD
 signing.secretKeyRingFile=${HOME}/.gnupg/secring.gpg
 EOF
 
-rm -rf /tmp/confluent/kafka-packaging
-mkdir -p /tmp/confluent
-git clone $MY_DIR/$REPOS_DIRECTORY/kafka-packaging.git /tmp/confluent/kafka-packaging
-pushd /tmp/confluent/kafka-packaging
+rm -rf $KAFKA_BUILD_ROOT
+mkdir -p $BUILD_ROOT
+git clone $MY_DIR/$REPOS_DIRECTORY/kafka-packaging.git $KAFKA_BUILD_ROOT
+pushd $KAFKA_BUILD_ROOT
 git remote add upstream $MY_DIR/$REPOS_DIRECTORY/kafka.git
 git fetch --tags upstream
-
 git checkout -b "deploy-$KAFKA_VERSION" origin/archive
 git merge --no-edit -m "deploy-$KAFKA_VERSION" upstream/$KAFKA_BRANCH
 make apply-patches # Note that this should also include the confluent-specific version # patch
 patch -p1 < ${MY_DIR}/patches/kafka-deploy.patch
 sed -i '' -e "s%REPOSITORY%s3://${MAVEN_BUCKET}${MAVEN_BUCKET_PREFIX}/maven%" \
     -e "s%SNAPSHOT_REPOSITORY%s3://${MAVEN_BUCKET}${MAVEN_BUCKET_PREFIX}/maven%" build.gradle
-gradle --gradle-user-home /tmp/fakegradlehome
-./gradlew --gradle-user-home /tmp/fakegradlehome uploadArchivesAll
+gradle --gradle-user-home $FAKE_GRADLE_HOME
+./gradlew --gradle-user-home $FAKE_GRADLE_HOME uploadArchivesAll
 popd
-rm -rf /tmp/confluent/kafka-packaging
+rm -rf $KAFKA_BUILD_ROOT
+rm -rf $FAKE_GRADLE_HOME
 
-rm -rf /tmp/fakegradlehome
 
-
-# The rest of the packages follow a simpler pattern
+###
+### Other CP (java-based) packages
+### The rest of the packages follow a simpler pattern
+###
 if [ -z "$BRANCH" ]; then
     BRANCH="$CONFLUENT_VERSION"
 fi
@@ -81,10 +90,10 @@ for PACKAGE in $JAVA_PACKAGES; do
         PACKAGE_SKIP_TESTS="$SKIP_TESTS"
     fi
 
-    mkdir -p /tmp/confluent
-    rm -rf /tmp/confluent/$PACKAGE
-    git clone $MY_DIR/$REPOS_DIRECTORY/$PACKAGE /tmp/confluent/$PACKAGE
-    pushd /tmp/confluent/$PACKAGE
+    mkdir -p $BUILD_ROOT
+    rm -rf $BUILD_ROOT/$PACKAGE
+    git clone $MY_DIR/$REPOS_DIRECTORY/$PACKAGE $BUILD_ROOT/$PACKAGE
+    pushd $BUILD_ROOT/$PACKAGE
     git checkout -b deploy $PACKAGE_BRANCH
     patch -p1 < ${MY_DIR}/patches/${PACKAGE}-deploy.patch
     MAVEN_OPTS=""
@@ -93,5 +102,5 @@ for PACKAGE in $JAVA_PACKAGES; do
     fi
     mvn $MAVEN_OPTS "-Dconfluent.release.repo=s3://${MAVEN_BUCKET}${MAVEN_BUCKET_PREFIX}/maven" "-Dconfluent.snapshot.repo=s3://${MAVEN_BUCKET}${MAVEN_BUCKET_PREFIX}/maven" clean deploy
     popd
-    rm -rf /tmp/confluent/$PACKAGE
+    rm -rf $BUILD_ROOT/$PACKAGE
 done
