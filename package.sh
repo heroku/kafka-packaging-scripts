46d3ee8430fc08f80f890cdf34f7e2d249349dad
Tom Crayford <tcrayford@googlemail.com>
2016-01-18 16:20:38 +0000

    kill unused tgz and rpm stuff
    https://github.com/heroku/kafka-packaging-scripts/commit/1431644bc85c4b0c5e988f8bc143755a35314ed4

--- package.sh
+++ package.sh
@@ -63,16 +63,6 @@ if [ "x$SIGN" == "xyes" ]; then
         SIGN_KEY=`gpg --list-secret-keys | grep uid | sed -e s/uid// -e 's/^ *//' -e 's/ *$//'`
     fi
 
-    cat <<EOF > .rpmmacros
-%_signature gpg
-%_gpg_path /root/.gnupg
-%_gpg_name $SIGN_KEY
-%_gpgbin /usr/bin/gpg
-EOF
-    vagrant ssh rpm -- sudo cp /vagrant/.rpmmacros /root/.rpmmacros
-    rm .rpmmacros
-fi
-
 ## KAFKA ##
 KAFKA_BUILD_SKIP_TESTS="yes"
 if [ ! -z "$kafka_SKIP_TESTS" ]; then
@@ -83,10 +73,6 @@ else
   fi
 fi
 
-vagrant ssh rpm -- cp /vagrant/build/kafka-archive.sh /tmp/kafka-archive.sh
-vagrant ssh rpm -- sudo VERSION=$KAFKA_VERSION REVISION=$REVISION BRANCH=$KAFKA_BRANCH "SCALA_VERSIONS=\"$SCALA_VERSIONS\"" "PS_ENABLED=\"$PS_ENABLED\"" "PS_PACKAGES=\"$PS_PACKAGES\"" "PS_CLIENT_PACKAGE=\"$PS_CLIENT_PACKAGE\"" "CONFLUENT_VERSION=\"$CONFLUENT_VERSION\"" SKIP_TESTS=$KAFKA_BUILD_SKIP_TESTS /tmp/kafka-archive.sh
-vagrant ssh rpm -- cp /vagrant/build/kafka-rpm.sh /tmp/kafka-rpm.sh
-vagrant ssh rpm -- -t sudo VERSION=$KAFKA_VERSION REVISION=$REVISION BRANCH=$KAFKA_BRANCH "SCALA_VERSIONS=\"$SCALA_VERSIONS\"" "PS_ENABLED=\"$PS_ENABLED\"" "PS_PACKAGES=\"$PS_PACKAGES\"" "PS_CLIENT_PACKAGE=\"$PS_CLIENT_PACKAGE\"" "CONFLUENT_VERSION=\"$CONFLUENT_VERSION\"" SKIP_TESTS=$KAFKA_BUILD_SKIP_TESTS SIGN=$SIGN /tmp/kafka-rpm.sh
 vagrant ssh deb -- cp /vagrant/build/kafka-deb.sh /tmp/kafka-deb.sh
 vagrant ssh deb -- -t sudo VERSION=$KAFKA_VERSION REVISION=$REVISION BRANCH=$KAFKA_BRANCH "SCALA_VERSIONS=\"$SCALA_VERSIONS\"" "PS_ENABLED=\"$PS_ENABLED\"" "PS_PACKAGES=\"$PS_PACKAGES\"" "PS_CLIENT_PACKAGE=\"$PS_CLIENT_PACKAGE\"" "CONFLUENT_VERSION=\"$CONFLUENT_VERSION\"" SKIP_TESTS=$KAFKA_BUILD_SKIP_TESTS SIGN=$SIGN /tmp/kafka-deb.sh
 
@@ -104,10 +90,6 @@ for PACKAGE in $CP_PACKAGES; do
         PACKAGE_SKIP_TESTS="$SKIP_TESTS"
     fi
 
-    vagrant ssh rpm -- cp "/vagrant/build/${PACKAGE}-archive.sh" "/tmp/${PACKAGE}-archive.sh"
-    vagrant ssh rpm -- sudo VERSION=$CONFLUENT_VERSION REVISION=$REVISION BRANCH=$PACKAGE_BRANCH SKIP_TESTS=$PACKAGE_SKIP_TESTS "/tmp/${PACKAGE}-archive.sh"
-    vagrant ssh rpm -- cp "/vagrant/build/${PACKAGE}-rpm.sh" "/tmp/${PACKAGE}-rpm.sh"
-    vagrant ssh rpm -- -t sudo VERSION=$CONFLUENT_VERSION REVISION=$REVISION BRANCH=$PACKAGE_BRANCH SKIP_TESTS=$PACKAGE_SKIP_TESTS SIGN=$SIGN "/tmp/${PACKAGE}-rpm.sh"
     vagrant ssh deb -- cp "/vagrant/build/${PACKAGE}-deb.sh" "/tmp/${PACKAGE}-deb.sh"
     vagrant ssh deb -- -t sudo VERSION=$CONFLUENT_VERSION REVISION=$REVISION BRANCH=$PACKAGE_BRANCH SKIP_TESTS=$PACKAGE_SKIP_TESTS SIGN=$SIGN "/tmp/${PACKAGE}-deb.sh"
 done
@@ -121,13 +103,6 @@ done
 # install. Finally, note that the BRANCH env variable isn't set for these --
 # there is no point since they have one fixed branch that they build from (rpm
 # or debian, stored in this repository).
-RPM_VERSION=`rpm_version_field $CONFLUENT_VERSION`
-RPM_RELEASE=`rpm_release_field $CONFLUENT_VERSION $REVISION`
-RPM_RELEASE_POSTFIX=`rpm_release_postfix $CONFLUENT_VERSION`
-KAFKA_RPM_VERSION=`rpm_version_field $KAFKA_VERSION`
-LIBRDKAFKA_RPM_VERSION=`rpm_version_field_librdkafka $LIBRDKAFKA_VERSION $CONFLUENT_VERSION`
-vagrant ssh rpm -- cp /vagrant/build/platform-rpm.sh /tmp/platform-rpm.sh
-vagrant ssh rpm -- -t sudo VERSION=$CONFLUENT_VERSION RPM_VERSION=$RPM_VERSION RPM_RELEASE=$RPM_RELEASE RPM_RELEASE_POSTFIX=$RPM_RELEASE_POSTFIX "SCALA_VERSIONS=\"$SCALA_VERSIONS\"" KAFKA_RPM_VERSION=$KAFKA_RPM_VERSION LIBRDKAFKA_RPM_VERSION=$LIBRDKAFKA_RPM_VERSION SIGN=$SIGN /tmp/platform-rpm.sh
 
 DEB_VERSION=`deb_version_field $CONFLUENT_VERSION $REVISION`
 KAFKA_DEB_VERSION=`deb_version_field $KAFKA_VERSION $REVISION`
@@ -143,42 +118,9 @@ pushd /tmp/confluent-packaging
 
 ABS_OUTPUT_DIRECTORY=${MY_DIR}/${OUTPUT_DIRECTORY}
 
-# zip/tar.gz
+# deb
 for SCALA_VERSION in $SCALA_VERSIONS; do
-    PACKAGE_ROOT="confluent-${CONFLUENT_VERSION}"
-    mkdir $PACKAGE_ROOT
-    pushd $PACKAGE_ROOT
-    tar -xz --strip-components 1 -f "${ABS_OUTPUT_DIRECTORY}/confluent-kafka-${KAFKA_VERSION}-${SCALA_VERSION}.tar.gz"
-    for PACKAGE in $CP_PACKAGES; do
-      if [ "$PACKAGE" = "librdkafka" ]; then
-        # TODO: librdkafka (and possibly further C-based packages) requires special treatment.
-        # Until we have identified a clear pattern for these "special" packages we hardcode
-        # this special-casing of librdkafka.
-        #
-        # Place the librdkafka tarball as-is (i.e. do not extract) under `src/`.
-        # The tarball contains the sources so there isn't a more appropriate place
-        # to handle the contents of this tarball.
-        LIBRDKAFKA_REL_DESTINATION_DIR="src/"
-        mkdir $LIBRDKAFKA_REL_DESTINATION_DIR
-        cp "${ABS_OUTPUT_DIRECTORY}/librdkafka-${LIBRDKAFKA_VERSION}_${CONFLUENT_VERSION}.tar.gz" $LIBRDKAFKA_REL_DESTINATION_DIR
-        cp "${ABS_OUTPUT_DIRECTORY}/librdkafka-${LIBRDKAFKA_VERSION}_${CONFLUENT_VERSION}.zip" $LIBRDKAFKA_REL_DESTINATION_DIR
-      else
-        # A "normal" CP packages like schema-registry.
-        tar -xz --strip-components 1 -f "${ABS_OUTPUT_DIRECTORY}/confluent-${PACKAGE}-${CONFLUENT_VERSION}.tar.gz"
-      fi
-    done
-    cp ${MY_DIR}/installers/README.archive .
-    popd
-    # TODO: the tar.gz should not contain the librdkafka zip file
-    tar -czf "${ABS_OUTPUT_DIRECTORY}/confluent-${CONFLUENT_VERSION}-${SCALA_VERSION}.tar.gz" $PACKAGE_ROOT
-    # TODO: the zip should not contain the librdkafka tar.gz file
-    zip -r "${ABS_OUTPUT_DIRECTORY}/confluent-${CONFLUENT_VERSION}-${SCALA_VERSION}.zip" $PACKAGE_ROOT
-    rm -rf $PACKAGE_ROOT
-done
-
-# deb/rpm
-for SCALA_VERSION in $SCALA_VERSIONS; do
-    for PKG_TYPE in "deb" "rpm"; do
+    for PKG_TYPE in "deb"; do
         PACKAGE_ROOT="confluent-${CONFLUENT_VERSION}"
         mkdir $PACKAGE_ROOT
         pushd $PACKAGE_ROOT
